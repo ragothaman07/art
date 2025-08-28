fb4eebf1eb5bb0f03d17cf6fcc48fc11b1bd4767
ragothaman07 <tvragothaman7@gmail.com>
2025-08-28 19:00:01 +0530

    update frontend1

--- python/phishing-react/src/App.jsx
+++ python/phishing-react/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+   const response = await fetch("https://your-backend.onrender.com/predict", {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- python/phishing-react/vite.config.js
+++ python/phishing-react/vite.config.js
@@ -3,16 +3,21 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    outDir: resolve(__dirname, '../phishing-python/dist'), // send build into backend
-    emptyOutDir: true, // clear before building
-  },
-  server: {
-    port: 5173, // dev server port
-    proxy: {
-      '/predict': 'http://127.0.0.1:5000', // forward API calls to Flask
+// Load env variables
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [tailwindcss(), react()],
+    build: {
+      outDir: resolve(__dirname, '../phishing-python/dist'), // build goes to backend
+      emptyOutDir: true,
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/predict': mode === 'development'
+          ? 'http://127.0.0.1:5000'   // local backend
+          : 'https://phishing-deduction-042k.onrender.com', // Render backend
+      }
     }
   }
 })
